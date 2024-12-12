0eb4689caa124180c089f240efe7b407492260d2
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2024-12-12 21:47:22 +0200

    project start commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace Pokemon_revolution_online_bot
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
